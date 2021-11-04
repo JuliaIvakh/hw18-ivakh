b4dabac8f252c1c07493424821ca0187f9acc403
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-11-04 17:20:45 +0200

    hw18

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let obj = {
+  name: "Vlad",
+  next: {
+    name: "Lina",
+    next: {
+      name: "Valentina",
+      next: {
+        name: "Yuri",
+        next: {
+          name: "Heorhii",
+        },
+      },
+    },
+  },
+};
+
+function findName(object) {}
+findName(obj);
